41a913ab3afca9ef11c6b79393c19271a7a14131
9swamp9god9 <roman.lazarev.ru@gmail.com>
2026-04-07 19:32:52 +0300

    sprint 3:Обновленный config

--- app/config.py
+++ app/config.py
@@ -2,8 +2,19 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    API_KEY: str
+
+    API_KEY: str = "ml_secret_key"
+
     DEVICE: str = "cpu"
+
+    TOXIC_THRESHOLD: float = 0.65
+
+    MAX_TEXT_LENGTH: int = 300
+
+    TTS_SAMPLE_RATE: int = 48000
+
+    TTS_SPEAKER: str = "aidar"
+
 
     class Config:
         env_file = ".env"
