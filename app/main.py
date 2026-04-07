a51c79b4d156770623df3e190b53d534417c085e
9swamp9god9 <roman.lazarev.ru@gmail.com>
2026-04-07 19:33:27 +0300

    sprint 3: Обновленный main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,40 @@
 from fastapi import FastAPI
-
-from app.api.tts import router
+from app.api.tts import router as tts
+from app.api.moderation import router as moderation
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request
 
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(tts)
+
+app.include_router(moderation)
 
 
 @app.get("/health")
+
 async def health():
+
     return {"status": "ok"}
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(
+    request: Request,
+    exc: Exception
+):
+
+    return JSONResponse(
+
+        status_code=500,
+
+        content={
+
+            "error": True,
+
+            "code": "INTERNAL_ERROR",
+
+            "message": "Internal server error"
+        }
+    )
