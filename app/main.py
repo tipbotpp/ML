d692f49426dbc39237a70bb4c0086923b0eafca0
илья кинке <ilya@qu1nqqy.ru>
2026-04-09 12:06:33 +0300

    feat(app): добавил поддержку lifespan для приложения FastAPI

--- app/main.py
+++ app/main.py
@@ -1,40 +1,47 @@
+import traceback
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+
 from app.api.tts import router as tts
 from app.api.moderation import router as moderation
-from fastapi.responses import JSONResponse
-from fastapi.requests import Request
+from app.middlewares.logging import HTTPLoggingMiddleware
+from app.services.logger import get_logger
+from app.services.s3 import S3Client
+
+logger = get_logger()
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with S3Client() as s3:
+        app.state.s3 = s3
+        yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(HTTPLoggingMiddleware)
 
 app.include_router(tts)
-
 app.include_router(moderation)
 
 
 @app.get("/health")
-
 async def health():
-
     return {"status": "ok"}
 
 
 @app.exception_handler(Exception)
-async def global_exception_handler(
-    request: Request,
-    exc: Exception
-):
-
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.error("Unhandled exception", error=str(exc), traceback=traceback.format_exc())
     return JSONResponse(
-
         status_code=500,
-
         content={
-
             "error": True,
-
             "code": "INTERNAL_ERROR",
-
-            "message": "Internal server error"
-        }
+            "message": "Internal server error",
+        },
     )
