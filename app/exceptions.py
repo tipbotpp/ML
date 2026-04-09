5bdfb8da2c158dd005906d4a9365106ef717e041
илья кинке <ilya@qu1nqqy.ru>
2026-04-09 12:06:30 +0300

    refactor(exceptions): удалил ненужные исключения из кода

--- app/exceptions.py
+++ app/exceptions.py
@@ -21,65 +21,13 @@ class MLException(HTTPException):
             }
         )
 
-class ToxicityException(MLException):
-
-    def __init__(self, score: float):
-
-        super().__init__(
-
-            code="TOXIC_MESSAGE",
-
-            message="Сообщение содержит токсичность",
-
-            details={
-                "toxicity_score": score
-            },
-
-            status_code=400
-        )
-
-
-class StopWordException(MLException):
-
-    def __init__(self, word: str):
-
-        super().__init__(
-
-            code="STOP_WORD",
-
-            message="Сообщение содержит запрещеное слово",
-
-            details={
-                "word": word
-            },
-
-            status_code=400
-        )
-
-
-class TextTooLongException(MLException):
-
-    def __init__(self):
-
-        super().__init__(
-
-            code="TEXT_TOO_LONG",
-
-            message="Сообщение слишком длинное",
-
-            status_code=400
-        )
-
 
 class TTSGenerationException(MLException):
 
     def __init__(self):
 
         super().__init__(
-
             code="TTS_ERROR",
-
             message="TTS generation failed",
-
             status_code=500
         )
